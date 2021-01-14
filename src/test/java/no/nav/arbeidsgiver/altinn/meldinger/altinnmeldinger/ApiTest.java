93531dd5f4e7582afed3846ba53d4c2a555e3a9a
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-14 16:46:41 +0100

    Skriv API-test som sjekker hele verdikjeden

---
+++ src/test/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/ApiTest.java
@@ -0,0 +1,36 @@
+package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static java.net.http.HttpClient.newBuilder;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"wiremock.port=8082"})
+public class ApiTest {
+
+    @LocalServerPort
+    private String port;
+
+    @Test
+    public void api__skal_sende_melding_via_ws_og_returnere_created() throws Exception {
+        HttpResponse<String> response = newBuilder().build().send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:" + port + "/altinn-meldinger/melding"))
+                        .POST(HttpRequest.BodyPublishers.ofString("Dette er en melding som skal til Altinn"))
+                        .build(),
+                ofString()
+        );
+
+        assertThat(response.statusCode()).isEqualTo(201);
+    }
+
+}
