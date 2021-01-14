bcb9c01992502c1c00df8f91eb6bda5d02cce25f
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-14 10:45:11 +0100

    Initial commit
    Co-authored-by: Torstein Gjengedal <torstein.gjengedal@nav.no>

---
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplication.java
@@ -0,0 +1,13 @@
+package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltinnMeldingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltinnMeldingerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltinnMeldingerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
