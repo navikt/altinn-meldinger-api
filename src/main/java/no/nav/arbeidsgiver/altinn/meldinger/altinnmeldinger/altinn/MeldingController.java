9c5d6616fd7ef6056c93aca7b15aee8cd933efc5
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-19 10:24:27 +0100

    Disable controller for prod

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/MeldingController.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/MeldingController.java
@@ -1,12 +1,13 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile({"local", "dev-gcp"})
 @RestController
 public class MeldingController {
 
