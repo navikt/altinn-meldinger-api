ff635419bc14f6d9772eb32234b60c5c2033624b
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-25 14:27:00 +0100

    Cleanup

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/MeldingLoggRepository.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/MeldingLoggRepository.java
@@ -1,7 +1,6 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn;
 
 import de.huxhorn.sulky.ulid.ULID;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeldingLoggRepository extends JpaRepository<MeldingLogg, ULID> {
