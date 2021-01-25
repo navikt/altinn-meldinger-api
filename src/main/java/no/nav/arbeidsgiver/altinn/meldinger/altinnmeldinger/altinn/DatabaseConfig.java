0decf5772e91b69233d05398434d9815f1fdb107
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-25 10:48:34 +0100

    Konfigurer datsource og flyway eksplisitt

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/DatabaseConfig.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/DatabaseConfig.java
@@ -1,17 +1,50 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
-    private final String jdbcUrl;
     private final static Logger log = LoggerFactory.getLogger(DatabaseConfig.class);
+    private final String url;
+    private final String username;
+    private final String password;
 
-    public DatabaseConfig(@Value("${spring.datasource.url}") String jdbcUrl) {
-        this.jdbcUrl = jdbcUrl;
-        log.info("JDBC-url: " + jdbcUrl);
+    public DatabaseConfig(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password
+    ) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(2);
+        config.setMinimumIdle(1);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> Flyway.configure()
+                .dataSource(dataSource())
+                .load()
+                .migrate();
     }
 }
