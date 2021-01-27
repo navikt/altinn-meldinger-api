c0e5383627ec2cbbc475302ee6be9b68aa181d22
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-01-27 14:57:34 +0100

    Log flyway locations

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/DatabaseConfig.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/DatabaseConfig.java
@@ -2,7 +2,10 @@ package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.juli.logging.LogFactory;
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,8 @@ import javax.sql.DataSource;
 @Profile({"dev-gcp", "prod-gcp"})
 @Configuration
 public class DatabaseConfig {
+    private final static Logger log = LoggerFactory.getLogger(DatabaseConfig.class);
+
     private final String url;
     private final String username;
     private final String password;
@@ -21,11 +26,13 @@ public class DatabaseConfig {
     public DatabaseConfig(
             @Value("${spring.datasource.url}") String url,
             @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password
+            @Value("${spring.datasource.password}") String password,
+            @Value("${spring.flyway.locations}") String locations
     ) {
         this.url = url;
         this.username = username;
         this.password = password;
+        log.info("spring.flyway.locations " + locations);
     }
 
     @Bean
