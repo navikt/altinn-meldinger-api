54aa0dfd87a8ac054b0519b2bff6c81e999a4315
bjartty <bjarte.tynning@nav.no>
2021-01-27 13:33:39 +0100

    azure-test

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplikasjon.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplikasjon.java
@@ -1,9 +1,11 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger;
 
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableOAuth2Client(cacheEnabled = true)
 public class AltinnMeldingerApplikasjon {
 
 	public static void main(String[] args) {
