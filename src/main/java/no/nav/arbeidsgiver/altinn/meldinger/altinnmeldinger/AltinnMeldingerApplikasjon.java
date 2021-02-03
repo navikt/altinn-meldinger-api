c1fe70ebfc6d078f4de6460ef366090d1a8cd541
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2021-02-03 14:35:17 +0100

    Enable token validation i dev/prod

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplikasjon.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/AltinnMeldingerApplikasjon.java
@@ -1,8 +1,10 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger;
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJwtTokenValidation(ignore = {"org.springframework", "org.springdoc"})
 @SpringBootApplication
 public class AltinnMeldingerApplikasjon {
 
