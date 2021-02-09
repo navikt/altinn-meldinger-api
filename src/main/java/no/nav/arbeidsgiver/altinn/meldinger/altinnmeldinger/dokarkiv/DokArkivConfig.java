d5f917a5a98c14988c4ac4245c5a0c261ce6d7dd
bjartty <bjarte.tynning@nav.no>
2021-02-09 12:35:51 +0100

    Ny RestTemplate

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/dokarkiv/DokArkivConfig.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/dokarkiv/DokArkivConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -31,6 +32,13 @@ public class DokArkivConfig {
 
     public String getPdfGenUri() {
         return pdfGenUri;
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
+        return restTemplate;
     }
 
     @Bean
