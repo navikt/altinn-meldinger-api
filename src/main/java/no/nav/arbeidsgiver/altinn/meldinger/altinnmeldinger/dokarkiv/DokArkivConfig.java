c2bf40b712fb6595eccd40eaedfcb3db73fd7891
bjartty <bjarte.tynning@nav.no>
2021-02-09 13:42:48 +0100

    builder

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/dokarkiv/DokArkivConfig.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/dokarkiv/DokArkivConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -35,12 +36,9 @@ public class DokArkivConfig {
     }
 
     @Bean
-    RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(5000);
-        restTemplate.setRequestFactory(factory);
-        return restTemplate;
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .customizers(restTemplate -> restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))).build();
     }
 
     @Bean
