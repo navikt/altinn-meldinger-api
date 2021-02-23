e3531f970e85fe56272458500229d0f0be413cce
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-02-23 15:43:27 +0100

    Lagt til metrics for utsendinger og arkivering

--- src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/utsending/AltinnService.java
+++ src/main/java/no/nav/arbeidsgiver/altinn/meldinger/altinnmeldinger/altinn/utsending/AltinnService.java
@@ -1,5 +1,8 @@
 package no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn.utsending;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn.MeldingRepository;
 import no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn.domene.AltinnStatus;
 import no.nav.arbeidsgiver.altinn.meldinger.altinnmeldinger.altinn.domene.MeldingsProsessering;
@@ -18,13 +21,32 @@ public class AltinnService {
     private final static Logger log = LoggerFactory.getLogger(AltinnService.class);
 
     private final MeldingRepository meldingRepository;
+    private final MeterRegistry meterRegistry;
     private final AltinnClient altinnClient;
     private final ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5,
             0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
 
-    public AltinnService(MeldingRepository meldingRepository, AltinnClient altinnClient) {
+    private final Counter altinnOKCounter;
+    private final Counter altinnFailedCounter;
+    private final Counter altinnIgnoredCounter;
+    private final Timer altinnTimer;
+
+    public AltinnService(MeldingRepository meldingRepository, MeterRegistry meterRegistry, AltinnClient altinnClient) {
         this.meldingRepository = meldingRepository;
+        this.meterRegistry = meterRegistry;
         this.altinnClient = altinnClient;
+
+        altinnOKCounter = Counter.builder("altinn-meldinger-api.melding-sendt.ok.count")
+                .description("Antall meldinger sendt OK til Altinn")
+                .register(meterRegistry);
+        altinnFailedCounter = Counter.builder("altinn-meldinger-api.melding-sendt.feilet.count")
+                .description("Antall meldinger sendt OK til Altinn")
+                .register(meterRegistry);
+        altinnIgnoredCounter = Counter.builder("altinn-meldinger-api.melding-sendt.ikke-prosessert.count")
+                .description("Antall meldinger sendt OK til Altinn")
+                .register(meterRegistry);
+        altinnTimer = Timer.builder("altinn-meldinger-api.melding-sendt.timer")
+                .register(meterRegistry);
     }
 
     public void sendNyeAltinnMeldinger() {
@@ -35,25 +57,34 @@ public class AltinnService {
     }
 
     private void sendTilAltinn(List<MeldingsProsessering> meldinger) {
-        List<Callable<Pair<String, AltinnStatus>>> callables = meldinger.stream()
-                .map(this::callable)
-                .collect(Collectors.toList());
-
-        try {
-            List<Future<Pair<String, AltinnStatus>>> futures = executor.invokeAll(callables,280, TimeUnit.SECONDS);
-            List<Optional<Pair<String, AltinnStatus>>> optionalStatus = futures.stream()
-                    .map(AltinnService::mapTilStatusPar)
+        altinnTimer.record(() -> {
+            List<Callable<Pair<String, AltinnStatus>>> callables = meldinger.stream()
+                    .map(this::callable)
                     .collect(Collectors.toList());
 
-            log.info("Sending til Altinn fullført. Antall sendt ok: {}, antall sendt med feil {}, antall ikke prosessert {}",
-                    optionalStatus.stream().filter(Optional::isPresent).map(Optional::get).filter(pair -> AltinnStatus.OK.equals(pair.getRight())).count(),
-                    optionalStatus.stream().filter(Optional::isPresent).map(Optional::get).filter(pair -> AltinnStatus.FEIL.equals(pair.getRight())).count(),
-                    optionalStatus.stream().filter(Optional::isEmpty).count()
-            );
+            try {
+                List<Future<Pair<String, AltinnStatus>>> futures = executor.invokeAll(callables,280, TimeUnit.SECONDS);
+                List<Optional<Pair<String, AltinnStatus>>> optionalStatus = futures.stream()
+                        .map(AltinnService::mapTilStatusPar)
+                        .collect(Collectors.toList());
 
-        } catch (InterruptedException e) {
-            log.warn("Kjøring avbrutt", e);
-        }
+                long antallSendtOk = optionalStatus.stream().filter(Optional::isPresent).map(Optional::get).filter(pair -> AltinnStatus.OK.equals(pair.getRight())).count();
+                long antallSendtMedFeil = optionalStatus.stream().filter(Optional::isPresent).map(Optional::get).filter(pair -> AltinnStatus.FEIL.equals(pair.getRight())).count();
+                long antallIkkeProsessert = optionalStatus.stream().filter(Optional::isEmpty).count();
+                altinnOKCounter.increment(antallSendtOk);
+                altinnFailedCounter.increment(antallSendtMedFeil);
+                altinnIgnoredCounter.increment(antallIkkeProsessert);
+
+                log.info("Sending til Altinn fullført. Antall sendt ok: {}, antall sendt med feil {}, antall ikke prosessert {}",
+                        antallSendtOk,
+                        antallSendtMedFeil,
+                        antallIkkeProsessert
+                );
+
+            } catch (InterruptedException e) {
+                log.warn("Kjøring avbrutt", e);
+            }
+        });
     }
 
     private static Optional<Pair<String, AltinnStatus>> mapTilStatusPar(Future<Pair<String, AltinnStatus>> pairFuture) {
